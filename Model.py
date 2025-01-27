ece5c91fdb47ea1885c40dc2d62e6c15db7f20db
Tony <apstone45@gmail.com>
2025-01-26 22:38:57 -0500

    model finished

---
+++ Model.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split as tts, KFold as KF
+from sklearn.ensemble import RandomForestClassifier as RFC, RandomForestRegressor as RFR
+from sklearn.metrics import accuracy_score as ac, root_mean_squared_error as rmse, RocCurveDisplay as rcd, PredictionErrorDisplay as ped, r2_score as r2
+from mapie.regression import MapieRegressor as MP
+import matplotlib.pyplot as plt
+
+def getIntervals(actual, predictions):
+    residuals = np.abs(actual - predictions)
+    y_ub = predictions + residuals
+    y_lb = predictions - residuals
+    return y_ub, y_lb
+
+def getResiduals(actual, predictions):
+    residuals = np.abs(actual - predictions)
+    return residuals
+
+def countries(data, modelPlot = True, futureData = None, predict = ''):
+    #Regression model for country data
+    country_y = data[predict]
+    country_x = data.drop(axis = 1, labels = ['Gold', 'Silver', 'Bronze', 'Total']).map(hash)  #split data
+
+    x_train, x_test, y_train, y_test = tts(country_x, country_y, train_size =.8, random_state=2)
+
+    model_rfr = RFR(n_estimators = 200, random_state=2)
+    model_rfr.fit(x_train, y_train)
+
+    y_pred = model_rfr.predict(x_test)
+    #y_pred = pd.DataFrame(y_pred)
+
+    a = rmse(y_test, y_pred)
+    b = r2(y_test, y_pred)
+
+    print(f"{predict}\nRoot means squared: {a} \nR2 Score: {b}")
+
+    if modelPlot == True:
+        y_ub, y_lb = getIntervals(y_test, y_pred)
+        
+        #sns.lmplot(x=y_pred, y=y_test, ci = 95)
+        plt.scatter(y_pred, y_test, color= 'blue', label = 'Predictions')
+        plt.scatter(y_lb, y_test, color= 'red', label = 'Lower Bound')
+        plt.scatter(y_ub, y_test, color= 'green', label = 'Upper Bound')
+        plt.title(f"Future {predict} medals")
+        plt.xlabel('Predicted Values')
+        plt.ylabel('Actual Values')
+        plt.show()
+    
+    if futureData is not None:
+        futureData.drop(axis=1, labels= 'Year')
+        future_pred = model_rfr.predict(futureData.map(hash))
+
+        plt.figure(figsize=(20,10))
+        plt.bar(x=futureData['NOC'], height=future_pred, color='gray')
+
+        plt.title(f'Future {predict} Predictions')
+        plt.xlabel('competing countries')
+        plt.ylabel('predicted medal counts')
+        plt.xticks(rotation = 80)
+        plt.show()
+
+        predictions = {predict : future_pred}
+        pred_df = pd.DataFrame(predictions)
+        
+        pred_df.to_csv(f'2025_Problem_C_Data/2025_Problem_C_Data/{predict}.csv', index = False)
--- runSpace.py
+++ runSpace.py
@@ -1,7 +1,11 @@
-from Models import *
-
-athleteD = pd.read_csv('2025_Problem_C_Data/2025_Problem_C_Data/summerOly_athletes.csv')
+from Model import *
 
 countryD = pd.read_csv('2025_Problem_C_Data/2025_Problem_C_Data/summerOly_medal_counts.csv')
+futureData = pd.read_csv('2025_Problem_C_Data/2025_Problem_C_Data/2028Rankings.csv')
 
-countries(countryD, 2)
+futureSorted = futureData.sort_values(by= 'Rank')
+
+medals = ['Gold', 'Silver', 'Bronze', 'Total']
+
+for medal in medals:
+    countries(countryD, futureData=futureSorted, predict=medal)
